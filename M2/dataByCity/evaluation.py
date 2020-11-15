3601ac2b75e69d2f2696cf547fcca510e1b956a2
JiaomaWHU <jiaomajiaoma@hotmail.com>
2020-11-14 20:50:30 -0600

    print result

--- M2/dataByCity/evaluation.py
+++ M2/dataByCity/evaluation.py
@@ -18,16 +18,19 @@ def evaluation(embeddings, sucessful_b_id):
     pca_embeddings = pca(embeddings)
 
     # total
-    inertia_v = inertia(pca_embeddings)
+    inertia_v_total = inertia(pca_embeddings)
+    print("distance metric of total network: {}".format(inertia_v_total))
 
     # success
-    inertia_v = inertia([pca_embeddings[i] for i in sucessful_b_id])
+    inertia_v_suc = inertia([pca_embeddings[i] for i in sucessful_b_id])
+    print("distance metric of success businesses network: {}".format(inertia_v_suc))
 
     # random
     ids = range(len(embeddings))
     for i in range(5):
         ids = random.shuffle(ids)
         inertia_v = inertia([pca_embeddings[i] for i in ids[:len(sucessful_b_id)]])
+        print("distance metric of random businesses network {}: {}".format(i, inertia_v))
 
 
 def pca(embeddings):
